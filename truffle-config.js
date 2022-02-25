5a44bde48d44e7ba87aaddcee4553b40fdbf155e
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2022-02-25 16:47:23 +0900

    Deploy test

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
+const fs = require("fs");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 //Account credentials from which our contract will be deployed
-const mnemonic = process.env.MNEMONIC;
+const PRIVATEKEY = fs.readFileSync("./.PRIVATEKEY").toString();
 
 //API key of your Datahub account for Avalanche Fuji test network
-const APIKEY = process.env.APIKEY;
+const APIKEY = fs.readFileSync("./.APIKEY").toString();
 const path = require("path");
 
 module.exports = {
@@ -18,14 +18,17 @@ module.exports = {
             port: 7545,
             network_id: "5777",
         },
+        ropsten: {
+            provider: function () {
+                return new HDWalletProvider(PRIVATEKEY, APIKEY);
+            },
+            network_id: 3,
+            gas: 4000000, //make sure this gas allocation isn't over 4M, which is the max
+        },
     },
     compilers: {
         solc: {
             version: "0.8.11",
-            optimizer: {
-                enabled: true,
-                runs: 200,
-            },
         },
     },
 };
